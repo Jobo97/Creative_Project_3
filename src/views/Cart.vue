6c080bb3c0127fcef57252769296c46a88d2752f
Jobo97 <ben.c.millett@gmail.com>
2020-11-07 15:19:52 -0700

    finished project

---
+++ src/views/Cart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="wrapper">Team Roster:</h1>
+    <RosterList :playersInfo="playersInfo" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<script>
+import RosterList from "../components/RosterList.vue";
+export default {
+  name: "Roster",
+  components: {
+    RosterList
+  },
+  computed: {
+    playersInfo() {
+      return this.$root.$data.players;
+    }
+  }
+};
+</script>
